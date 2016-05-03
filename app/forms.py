80841bb96c4b866f11781b93b3e94e1b131c9665
xxn59 <xxn59@gmail.com>
2016-05-03 16:43:17 +0800

    fix floor problem in Signup

--- app/forms.py
+++ app/forms.py
@@ -17,8 +17,8 @@ class SignupForm(Form):
     password = PasswordField('New password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField('Confirm new password', validators=[DataRequired()])
-    floor = SelectField(choices=[(8, 8), (9, 9), (10, 10), (11, 11), (14, 14), (15, 15), (16, 16), (17, 17),
-                                 (18, 18), (19, 19), (20, 20), (21, 21), (22, 22), (23, 23), (24, 24)],
+    floor = SelectField(choices=[('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('14', '14'), ('15', '15'), ('16', '16'), ('17', '17'),
+                                 ('18', '18'), ('19', '19'), ('20', '20'), ('21', '21'), ('22', '22'), ('23', '23'), ('24', '24')],
                         validators=[DataRequired()])
     gender = SelectField(choices=[('female', u'妹子'), ('male', u'汉子'), ('malegirl', u'男妹子'), ('femaleboy', u'女汉子')])
 
